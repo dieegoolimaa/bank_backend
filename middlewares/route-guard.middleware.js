9897c930db218328fcff950438c849cc28aa36c2
Diego Fernandes <dieegoolimaa@gmail.com>
2024-11-17 16:32:35 +0000

    change middleware code

--- middlewares/route-guard.middleware.js
+++ middlewares/route-guard.middleware.js
@@ -1,16 +1,17 @@
 const JWT = require("jsonwebtoken");
 
 const isAuthenticated = (req, res, next) => {
-  const token = req.cookies.authToken;
-  if (!token) return res.status(401).send("No token provided");
-
   try {
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    res.json({
-      message: `Welcome, ${user.username}! You are logged in and authorized to access this route`,
-    });
-  } catch {
-    res.status(403).send("Token not valid");
+    const token = req.cookies.authToken;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
 
