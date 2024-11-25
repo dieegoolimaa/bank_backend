3983bc0887e83956e9fa42ae929d9e196c874da9
Diego Fernandes <dieegoolimaa@gmail.com>
2024-11-25 21:26:34 +0000

    update code

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,25 +3,6 @@ import Profile from "../models/Profile.model.js";
 import User from "../models/User.model.js";
 const router = express.Router();
 
-{
-  /* const mongoose = require("mongoose");
-
-const profileSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  postalCode: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Profile", profileSchema); */
-}
-
 // Profile creation
 router.post("/", isAuthenticated, async (req, res) => {
   try {
